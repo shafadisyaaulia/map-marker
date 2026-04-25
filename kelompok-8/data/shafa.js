f625ed10a544d202cfda939d2ff7c15850a7f7af
shafadisya <shafadisya@mhs.usk.ac.id>
2026-04-25 22:57:08 +0700

    Day 1: data titik warung kopi Aceh

---
+++ kelompok-8/data/shafa.js
@@ -0,0 +1,8 @@
+const lokasiMaulizar = [
+    { nama: "Solong Premium Coffee Ulee Kareng", lat: 5.545230, lng: 95.332890 },
+    { nama: "Dhapu Kupi", lat: 5.553910, lng: 95.319870 },
+    { nama: "Zakir Warkop", lat: 5.548350, lng: 95.320930 },
+    { nama: "Kupi Nanggroe", lat: 5.547020, lng: 95.324780 },
+    { nama: "Canai Mamak KL", lat: 5.550640, lng: 95.317220 },
+    { nama: "Rumoh Kupi Premium", lat: 5.546180, lng: 95.328540 }
+];
