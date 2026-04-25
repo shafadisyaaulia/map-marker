dbab3fc88c4eaffcca4069e396bab3ae1b909727
Maulizar <maulizar23@mhs.usk.ac.id>
2026-04-25 21:36:36 +0700

    menambah data koordinat lokasi di maulizar.js

--- kelompok-8/data/maulizar.js
+++ kelompok-8/data/maulizar.js
@@ -1,17 +1,32 @@
 const lokasiMaulizar = [
   {
-    nama: "Jl. Moh. Jam No.1, Kp. Baru, Kec. Baiturrahman, Kota Banda Aceh, Aceh 23127",
+    nama: "Masjid Raya Baiturrahman, Jl. Moh. Jam No.1, Kp. Baru, Kec. Baiturrahman, Kota Banda Aceh, Aceh 23127",
     lat: 5.553905472864273,
     lng: 95.3177351853332
   },
   {
-    nama: "G8XC+5QC, Peuniti, Kec. Baiturrahman, Kota Banda Aceh, Aceh 23116",
+    nama: "Masjid Iskandar Muda, Peuniti, Kec. Baiturrahman, Kota Banda Aceh, Aceh 23116",
     lat: 5.548157039777464,
     lng: 95.32180544072799
   },
   {
-    nama: "H85V+PQ9, Jl. T. Panglima Nyak Makam, Ie Masen Kaye Adang, Kec. Syiah Kuala, Kota Banda Aceh, Aceh 24415",
+    nama: "Masjid Baitul Hasib Kantor BPK Aceh, Jl. T. Panglima Nyak Makam, Ie Masen Kaye Adang, Kec. Syiah Kuala, Kota Banda Aceh, Aceh 24415",
     lat: 5.560181401149151,
     lng: 95.34440083408327
+  },
+  {
+    nama: "Masjid Besar Pahlawan, Jl. TM Pahlawan, Peuniti, Kec. Baiturrahman, Kota Banda Aceh, Aceh 23116",
+    lat: 5.547024230331126,
+    lng: 95.32331160696903
+  },
+  {
+    nama: "Masjid Al-Fitrah Keraton, Kp. Baru, Kec. Baiturrahman, Kota Banda Aceh, Aceh 23116",
+    lat: 5.549330955841917,
+    lng: 95.31910573522796
+  },
+  {
+    nama: "Masjid Haji Keuchik Leumiek, Lamseupeung, Kec. Lueng Bata, Kota Banda Aceh, Aceh 23127",
+    lat: 5.550136535841128,
+    lng: 95.33658687157923
   }
 ];
