8885e714b3c765d238a8bc3b4d9760ffcef930c8
shafadisya <shafadisya@mhs.usk.ac.id>
2026-04-26 23:48:46 +0700

    Day 2: update lokasi Zakir Warkop

--- kelompok-8/data/shafa.js
+++ kelompok-8/data/shafa.js
@@ -1,7 +1,7 @@
 const lokasiShafa = [
     { nama: "Solong Premium Coffee Ulee Kareng", lat: 5.553576314633806, lng: 95.33444822386085, mapsUrl: "https://maps.app.goo.gl/DYVaYBr1i8chyt8f9" },
     { nama: "Dhapu Kupi", lat: 5.550917200541337, lng: 95.33041467967881, mapsUrl: "https://www.google.com/maps/search/?api=1&query=5.550917200541337,95.33041467967881" },
-    { nama: "Zakir Warkop", lat: 5.548350, lng: 95.320930, mapsUrl: "https://www.google.com/maps/search/?api=1&query=5.548350,95.320930" },
+    { nama: "Zakir Warkop", lat: 5.546936071967716, lng: 95.32341582698689, mapsUrl: "https://maps.app.goo.gl/9pWcbCh58nDNSrJh6" },
     { nama: "Kupi Nanggroe", lat: 5.547020, lng: 95.324780, mapsUrl: "https://www.google.com/maps/search/?api=1&query=5.547020,95.324780" },
     { nama: "Canai Mamak KL", lat: 5.550640, lng: 95.317220, mapsUrl: "https://www.google.com/maps/search/?api=1&query=5.550640,95.317220" },
     { nama: "Rumoh Kupi Premium", lat: 5.546180, lng: 95.328540, mapsUrl: "https://www.google.com/maps/search/?api=1&query=5.546180,95.328540" }
