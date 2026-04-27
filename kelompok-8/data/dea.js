9b4639578f2387b6d6e8e22ecd36d79c50cb3f4c
Dea Zasqia Pasaribu Malau <deazasqia@mhs.usk.ac.id>
2026-04-27 23:51:23 +0700

    menambah data sekolah aceh

---
+++ kelompok-8/data/dea.js
@@ -0,0 +1,34 @@
+if (!window.semuaLokasi) window.semuaLokasi = [];
+
+window.semuaLokasi.push(...[
+  {
+    nama: "SMA Negeri 1 Banda Aceh, Jl. Khairil Anwar, Kota Banda Aceh, Aceh",
+    lat: 5.548420,
+    lng: 95.319510
+  },
+  {
+    nama: "SMA Negeri 3 Banda Aceh, Jl. Tgk. Daud Beureueh, Kota Banda Aceh, Aceh",
+    lat: 5.556921,
+    lng: 95.331845
+  },
+  {
+    nama: "SMK Negeri 2 Banda Aceh, Jl. Sultan Malikul Saleh, Kota Banda Aceh, Aceh",
+    lat: 5.550942,
+    lng: 95.326104
+  },
+  {
+    nama: "MAN Model Banda Aceh, Jl. Pocut Baren, Kota Banda Aceh, Aceh",
+    lat: 5.553821,
+    lng: 95.327541
+  },
+  {
+    nama: "SMP Negeri 1 Banda Aceh, Jl. Prof. A. Majid Ibrahim, Kota Banda Aceh, Aceh",
+    lat: 5.547890,
+    lng: 95.320773
+  },
+  {
+    nama: "SD Negeri 20 Banda Aceh, Jl. Teuku Umar, Kota Banda Aceh, Aceh",
+    lat: 5.551662,
+    lng: 95.318244
+  }
+]);
