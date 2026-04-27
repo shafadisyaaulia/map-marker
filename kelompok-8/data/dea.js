66823fe06d56d6d015f35b3050ceb3c1bf166035
Dea Zasqia Pasaribu Malau <deazasqia@mhs.usk.ac.id>
2026-04-28 00:14:41 +0700

    update koordinat sekolah banda aceh

--- kelompok-8/data/dea.js
+++ kelompok-8/data/dea.js
@@ -2,33 +2,33 @@ if (!window.semuaLokasi) window.semuaLokasi = [];
 
 window.semuaLokasi.push(...[
   {
-    nama: "SMA Negeri 1 Banda Aceh, Jl. Khairil Anwar, Kota Banda Aceh, Aceh",
-    lat: 5.548420,
-    lng: 95.319510
+    nama: "SD Negeri 1 Banda Aceh, Jl. Prof. A. Majid Ibrahim I No.23, Merduati, Kec. Kuta Raja, Kota Banda Aceh, Aceh",
+    lat: 5.5528423,
+    lng: 95.3130464
   },
   {
-    nama: "SMA Negeri 3 Banda Aceh, Jl. Tgk. Daud Beureueh, Kota Banda Aceh, Aceh",
-    lat: 5.556921,
-    lng: 95.331845
+    nama: "SD Negeri 20 Banda Aceh, Jl. Poecut Baren No.67, Kampung Keramat, Kota Banda Aceh, Aceh",
+    lat: 5.5620988,
+    lng: 95.3232910
   },
   {
-    nama: "SMK Negeri 2 Banda Aceh, Jl. Sultan Malikul Saleh, Kota Banda Aceh, Aceh",
-    lat: 5.550942,
-    lng: 95.326104
+    nama: "SMP Negeri 1 Banda Aceh, Punge Jurong, Kec. Meuraxa, Kota Banda Aceh, Aceh",
+    lat: 5.5521015,
+    lng: 95.3127823
   },
   {
-    nama: "MAN Model Banda Aceh, Jl. Pocut Baren, Kota Banda Aceh, Aceh",
-    lat: 5.553821,
-    lng: 95.327541
+    nama: "SMP Negeri 3 Banda Aceh, Jl. Teuku Lamu No.1, Neusu Jaya, Kec. Baiturrahman, Kota Banda Aceh, Aceh",
+    lat: 5.5447399,
+    lng: 95.3182964
   },
   {
-    nama: "SMP Negeri 1 Banda Aceh, Jl. Prof. A. Majid Ibrahim, Kota Banda Aceh, Aceh",
-    lat: 5.547890,
-    lng: 95.320773
+    nama: "SMA Negeri 1 Banda Aceh, Jl. Prof. A. Majid Ibrahim II No.1, Punge Jurong, Kec. Meuraxa, Kota Banda Aceh, Aceh",
+    lat: 5.5513700,
+    lng: 95.3124848
   },
   {
-    nama: "SD Negeri 20 Banda Aceh, Jl. Teuku Umar, Kota Banda Aceh, Aceh",
-    lat: 5.551662,
-    lng: 95.318244
+    nama: "SMA Negeri 3 Banda Aceh, Jl. Tgk. Daud Beureuh No.454, Bandar Baru, Kec. Kuta Alam, Kota Banda Aceh, Aceh",
+    lat: 5.5610931,
+    lng: 95.3317178
   }
 ]);
