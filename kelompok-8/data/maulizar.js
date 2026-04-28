499ea916a729161099e4c6b773eaf21417408de0
Maulizar <maulizar23@mhs.usk.ac.id>
2026-04-28 21:35:28 +0700

    Menambahkan beberapa titik koordinat

--- kelompok-8/data/maulizar.js
+++ kelompok-8/data/maulizar.js
@@ -2,48 +2,178 @@ if (!window.semuaLokasi) window.semuaLokasi = [];
 
 window.semuaLokasi.push(...[
   {
-    nama: "Masjid Raya Baiturrahman, Jl. Moh. Jam No.1, Kp. Baru, Kec. Baiturrahman, Kota Banda Aceh, Aceh 23127",
-    lat: 5.5536492,
-    lng: 95.3177781
+    nama: "Masjid Raya Baiturrahman",
+    alamat: "Jl. Moh. Jam No.1, Kp. Baru, Banda Aceh",
+    lat: 5.553916868607073,
+    lng: 95.31779952354911,
+    url: "https://maps.app.goo.gl/RtuisdBGB4sv4vBv8"
   },
   {
-    nama: "Masjid Iskandar Muda, Peuniti, Kec. Baiturrahman, Kota Banda Aceh, Aceh 23116",
-    lat: 5.5479328,
-    lng: 95.321902
+    nama: "Masjid Iskandar Muda",
+    alamat: "Peuniti, Banda Aceh",
+    lat: 5.54814977219407,
+    lng: 95.32182490319565,
+    url: "https://maps.app.goo.gl/vdkq6u8giCsWSg9B8"
   },
   {
-    nama: "Masjid Baitul Hasib Kantor BPK Aceh, Jl. T. Panglima Nyak Makam, Ie Masen Kaye Adang, Kec. Syiah Kuala, Kota Banda Aceh, Aceh 24415",
-    lat: 5.5592847,
-    lng: 95.3444862
+    nama: "Masjid Baitul Hasib",
+    alamat: "Kantor BPK Aceh",
+    lat: 5.559466593133254,
+    lng: 95.34446105686894,
+    url: "https://maps.app.goo.gl/ChRYz942bZV1zFN1A"
   },
   {
-    nama: "Masjid Besar Pahlawan, Jl. TM Pahlawan, Peuniti, Kec. Baiturrahman, Kota Banda Aceh, Aceh 23116",
-    lat: 5.5463838,
-    lng: 95.3234399
+    nama: "Masjid Besar Pahlawan",
+    alamat: "Jl. TM Pahlawan",
+    lat: 5.54655630266166,
+    lng: 95.32345131378275,
+    url: "https://maps.app.goo.gl/m3xvfbBcAJU5KkQ26"
   },
   {
-    nama: "Masjid Al-Fitrah Keraton, Kp. Baru, Kec. Baiturrahman, Kota Banda Aceh, Aceh 23116",
-    lat: 5.5485876,
-    lng: 95.3192017
+    nama: "Masjid Al-Fitrah Keraton",
+    alamat: "Kp. Baru",
+    lat: 5.548763432473961,
+    lng: 95.31914972098902,
+    url: "https://maps.app.goo.gl/FU8x6eywMgjBWvXv5"
   },
   {
-    nama: "Masjid Haji Keuchik Leumiek, Lamseupeung, Kec. Lueng Bata, Kota Banda Aceh, Aceh 23127",
-    lat: 5.5497863,
-    lng: 95.3366611
+    nama: "Masjid Haji Keuchik Leumiek",
+    alamat: "Lamseupeung",
+    lat: 5.549811074542068,
+    lng: 95.33665859400035,
+    url: "https://maps.app.goo.gl/Lkcbw5ea38jEEmpS9"
   },
   {
-  nama: "Masjid Fathun Qarib UIN Ar-Raniry, Kopelma Darussalam, Kec. Syiah Kuala, Kota Banda Aceh, Aceh",
-  lat: 5.57761,
-  lng: 95.36923
+    nama: "Masjid Fathun Qarib UIN Ar-Raniry",
+    alamat: "Darussalam",
+    lat: 5.577619541230569,
+    lng: 95.3692574090854,
+    url: "https://maps.app.goo.gl/hSApZt5gWnDnyn1z5"
   },
   {
-  nama: "Masjid Baitul Muttaqin, Kopelma Darussalam, Kec. Syiah Kuala, Kota Banda Aceh, Aceh 24415",
-  lat: 5.57871,
-  lng: 95.36326
+    nama: "Masjid Baitul Muttaqin",
+    alamat: "Darussalam",
+    lat: 5.578753374460068,
+    lng: 95.36323488329103,
+    url: "https://maps.app.goo.gl/o1qdsaSScUNjyxRf6"
   },
   {
-  nama: "Masjid Jami' Darussalam, Kopelma Darussalam, Kec. Syiah Kuala, Kota Banda Aceh, Aceh 24415",
-  lat: 5.57133,
-  lng: 95.37143
+    nama: "Masjid Jami' Darussalam",
+    alamat: "Darussalam",
+    lat: 5.572502828310301,
+    lng: 95.37173082128436,
+    url: "https://maps.app.goo.gl/rBCydshpMgcNb7y76"
+  },
+  {
+    nama: "Masjid Al Hasyimiah",
+    alamat: "Rukoh",
+    lat: 5.573027966105124,
+    lng: 95.36212206238152,
+    url: "https://maps.app.goo.gl/GSWo9aBz4F6qfGmv8"
+  },
+  {
+    nama: "Masjid Al Furqan",
+    alamat: "Beurawe",
+    lat: 5.556298445753803,
+    lng: 95.33316560423634,
+    url: "https://maps.app.goo.gl/iUGvtayY9MSB1LdR9"
+  },
+  {
+    nama: "Masjid Al Huda",
+    alamat: "Laksana",
+    lat: 5.559945117390389,
+    lng: 95.32416142853404,
+    url: "https://maps.app.goo.gl/HZVwcoEvRfQRwc6F8"
+  },
+  {
+    nama: "Masjid Al Abrar",
+    alamat: "Lamdingin",
+    lat: 5.570418493859415,
+    lng: 95.32910809267962,
+    url: "https://maps.app.goo.gl/7J5obSaLxWnBhgby8"
+  },
+  {
+    nama: "Masjid Jami' Al-Hidayah",
+    alamat: "Peurada",
+    lat: 5.574725400610575,
+    lng: 95.34754701461073,
+    url: "https://maps.app.goo.gl/QJ3v63RhyCVtUccMA"
+  },
+  {
+    nama: "Masjid Babuttaqwa Polda Aceh",
+    alamat: "Jeulingke",
+    lat: 5.577202732038138,
+    lng: 95.34793330415901,
+    url: "https://maps.app.goo.gl/DH2CwsYNzRNu447a8"
+  },
+  {
+    nama: "Masjid Hidayatul Islam",
+    alamat: "Aceh Besar",
+    lat: 5.560912910887919,
+    lng: 95.36275061889707,
+    url: "https://maps.app.goo.gl/ETpX6R1wYUNtFs5H6"
+  },
+  {
+    nama: "Masjid Baitul Mukminin",
+    alamat: "Lamteh",
+    lat: 5.548994318554683,
+    lng: 95.34494370792264,
+    url: "https://maps.app.goo.gl/JySEkAWxobrttdJd7"
+  },
+  {
+    nama: "Masjid Jami' Lueng Bata",
+    alamat: "Lueng Bata",
+    lat: 5.54119161162964,
+    lng: 95.33576232104839,
+    url: "https://maps.app.goo.gl/LttpMStjBUt1f2g17"
+  },
+  {
+    nama: "Masjid Baiturrahim Ateuk Jawo",
+    alamat: "Ateuk Jawo",
+    lat: 5.5357347621887,
+    lng: 95.32163546910941,
+    url: "https://maps.app.goo.gl/syJ8jEFyoPnLd9EA7"
+  },
+  {
+    nama: "Masjid Baitul Musyahadah",
+    alamat: "Banda Raya",
+    lat: 5.536893411851292,
+    lng: 95.30713410504458,
+    url: "https://maps.app.goo.gl/yW43BvnuwuUvr2nX9"
+  },
+  {
+    nama: "Masjid Baiturrahmah",
+    alamat: "Jaya Baru",
+    lat: 5.534490680990468,
+    lng: 95.28876365652404,
+    url: "https://maps.app.goo.gl/HpaREK4AqUn15em46"
+  },
+  {
+    nama: "Masjid Hikmatullah",
+    alamat: "Peukan Bada",
+    lat: 5.529300709179089,
+    lng: 95.2909094117474,
+    url: "https://maps.app.goo.gl/2C22K95WxsDNfVi88"
+  },
+  {
+    nama: "Masjid Baitul Maghfirah",
+    alamat: "Peukan Bada",
+    lat: 5.534282445226789,
+    lng: 95.28058023628917,
+    url: "https://maps.app.goo.gl/GFPoFeCHMnL1J7Mx8"
+  },
+  {
+    nama: "Masjid Teungku Chik Mahraja Gurah",
+    alamat: "Peukan Bada",
+    lat: 5.532607200773616,
+    lng: 95.27378351849617,
+    url: "https://maps.app.goo.gl/fuj8fyuqgd6RU5xu7"
+  },
+  {
+    nama: "Masjid Baiturrahim Peukan Bada",
+    alamat: "Peukan Bada",
+    lat: 5.513848158457756,
+    lng: 95.27012230218816,
+    url: "https://maps.app.goo.gl/3zD2Fq7vzLBgYoXK6"
   }
 ]);
