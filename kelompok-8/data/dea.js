d2981ea9e324e3e796dfbd30c0b41f6e631b99d4
Dea Zasqia Pasaribu Malau <deazasqia@mhs.usk.ac.id>
2026-04-28 20:58:33 +0700

    menambah 22 lokasi sekolah banda aceh SD SMP SMA SMK MAN

--- kelompok-8/data/dea.js
+++ kelompok-8/data/dea.js
@@ -1,16 +1,38 @@
 if (!window.semuaLokasi) window.semuaLokasi = [];
 
 window.semuaLokasi.push(...[
+  // SD
   {
     nama: "SD Negeri 1 Banda Aceh, Jl. Prof. A. Majid Ibrahim I No.23, Merduati, Kec. Kuta Raja, Kota Banda Aceh, Aceh",
     lat: 5.5528423,
     lng: 95.3130464
   },
   {
+    nama: "SD Negeri 2 Banda Aceh, Jl. Prof. A. Majid Ibrahim II No.1, Punge Jurong, Kec. Meuraxa, Kota Banda Aceh, Aceh",
+    lat: 5.5523193,
+    lng: 95.311225
+  },
+  {
+    nama: "SD Negeri 3 Banda Aceh, Jl. Lintas Sumatera, Peuniti, Kec. Baiturrahman, Kota Banda Aceh, Aceh",
+    lat: 5.5513045,
+    lng: 95.32135
+  },
+  {
     nama: "SD Negeri 20 Banda Aceh, Jl. Poecut Baren No.67, Kampung Keramat, Kota Banda Aceh, Aceh",
     lat: 5.5620988,
     lng: 95.3232910
   },
+  {
+    nama: "SD Negeri 22 Banda Aceh, Jl. Nyak Adam Kamil III, Neusu Jaya, Kec. Baiturrahman, Kota Banda Aceh, Aceh",
+    lat: 5.5448171,
+    lng: 95.317686
+  },
+  {
+    nama: "SD Negeri 40 Banda Aceh, Peuniti, Kec. Baiturrahman, Kota Banda Aceh, Aceh",
+    lat: 5.5453513,
+    lng: 95.3199778
+  },
+  // SMP
   {
     nama: "SMP Negeri 1 Banda Aceh, Punge Jurong, Kec. Meuraxa, Kota Banda Aceh, Aceh",
     lat: 5.5521015,
@@ -22,6 +44,22 @@ window.semuaLokasi.push(...[
     lng: 95.3182964
   },
   {
+    nama: "SMP Negeri 4 Banda Aceh, Peunayong, Kec. Kuta Alam, Kota Banda Aceh, Aceh",
+    lat: 5.5612765,
+    lng: 95.3201194
+  },
+  {
+    nama: "SMP Negeri 7 Banda Aceh, Geuceu Kompleks, Kec. Banda Raya, Kota Banda Aceh, Aceh",
+    lat: 5.5334682,
+    lng: 95.3088112
+  },
+  {
+    nama: "SMP Negeri 17 Banda Aceh, Sukaramai, Kec. Baiturrahman, Kota Banda Aceh, Aceh",
+    lat: 5.5484843,
+    lng: 95.3137686
+  },
+  // SMA
+  {
     nama: "SMA Negeri 1 Banda Aceh, Jl. Prof. A. Majid Ibrahim II No.1, Punge Jurong, Kec. Meuraxa, Kota Banda Aceh, Aceh",
     lat: 5.5513700,
     lng: 95.3124848
@@ -30,5 +68,52 @@ window.semuaLokasi.push(...[
     nama: "SMA Negeri 3 Banda Aceh, Jl. Tgk. Daud Beureuh No.454, Bandar Baru, Kec. Kuta Alam, Kota Banda Aceh, Aceh",
     lat: 5.5610931,
     lng: 95.3317178
+  },
+  {
+    nama: "SMA Negeri 4 Banda Aceh, Jl. T. Panglima Nyak Makam No.19, Kota Baru, Kec. Kuta Alam, Kota Banda Aceh, Aceh",
+    lat: 5.566026,
+    lng: 95.34313
+  },
+  {
+    nama: "SMA Negeri 7 Banda Aceh, Jl. Krueng Jambo Aye No.1, Geuceu Komp., Kec. Banda Raya, Kota Banda Aceh, Aceh",
+    lat: 5.535039,
+    lng: 95.30861
+  },
+  {
+    nama: "SMA Negeri 9 Banda Aceh, Jl. Sultan Maliku Saleh, Lhong Raya, Kota Banda Aceh, Aceh",
+    lat: 5.5250349,
+    lng: 95.3250647
+  },
+  // SMK
+  {
+    nama: "SMK Negeri 1 Banda Aceh, Jl. Sultan Malikul Saleh, Lhong Raya, Kec. Banda Raya, Kota Banda Aceh, Aceh",
+    lat: 5.5217641,
+    lng: 95.3207916
+  },
+  {
+    nama: "SMK Negeri 2 Banda Aceh, Jl. Sultan Malikul Saleh, Lhong Raya, Kec. Banda Raya, Kota Banda Aceh, Aceh",
+    lat: 5.5223511,
+    lng: 95.3200010
+  },
+  {
+    nama: "SMK Negeri 3 Banda Aceh, Lhong Raya, Kec. Banda Raya, Kota Banda Aceh, Aceh",
+    lat: 5.5229381,
+    lng: 95.3204875
+  },
+  // MAN
+  {
+    nama: "MAN Model Banda Aceh, Jl. Pocut Baren No.116, Keuramat, Kota Banda Aceh, Aceh",
+    lat: 5.562461,
+    lng: 95.329809
+  },
+  {
+    nama: "MAN 2 Banda Aceh, Jl. Cut Nyak Dhien No.590, Lamtemen Bar., Kec. Jaya Baru, Kota Banda Aceh, Aceh",
+    lat: 5.5308130,
+    lng: 95.2980380
+  },
+  {
+    nama: "MAN Rukoh Banda Aceh, Jl. Lingkar Kampus, Kopelma Darussalam, Kec. Syiah Kuala, Kota Banda Aceh, Aceh",
+    lat: 5.5808476,
+    lng: 95.3636452
   }
 ]);
